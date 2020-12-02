6c6de4258e6773b4301d5cbd8b0abbedd578ff0f
sakibalamin <sakibalamin@appscode.com>
2020-12-02 17:29:49 +0600

    first commit from subfunc branch

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ func sum(x, y int) int {
 	return x + y
 }
 
+func sub(x, y int) int {
+	return x - y
+}
+
 func main() {
 	fmt.Println("Master Branch!")
 	fmt.Println(sum(10, 20))
+	fmt.Println(sub(20, 10))
 }
